207ae8c32e1a90298b271f3c083b45593d976102
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-21 19:54:01 -0300

    example:get clerk's user data in client component

---
+++ app/(platform)/protected/client/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useAuth, useUser } from "@clerk/nextjs";
+
+export default function ProtectedClientPage() {
+  const { user } = useUser();
+  const { userId } = useAuth();
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Protected Client Page</h1>
+      <p>useUser User: {user?.firstName}</p>
+      <p>useAuth userId: {userId}</p>
+    </div>
+  );
+}
--- app/(platform)/protected/server/page.tsx
+++ app/(platform)/protected/server/page.tsx
@@ -1,12 +1,12 @@
 import { auth, currentUser } from "@clerk/nextjs";
 
-export default async function ProtectedPage() {
+export default async function ProtectedServerPage() {
   const user = await currentUser();
   const { userId, user: authUser } = auth();
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">Protected Page</h1>
+      <h1 className="text-3xl font-bold">Protected Server Page</h1>
       <p>User: {user?.firstName}</p>
       <p>UserId: {userId}</p>
       <p>authUser: {authUser?.lastName}</p>
