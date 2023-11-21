84be330157e235374b7c04d374a0bf47cb08937c
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-21 19:57:51 -0300

    example:add callback url after sign-out

--- app/(platform)/protected/client/page.tsx
+++ app/(platform)/protected/client/page.tsx
@@ -11,7 +11,7 @@ export default function ProtectedClientPage() {
       <h1 className="text-3xl font-bold">Protected Client Page</h1>
       <p>useUser User: {user?.firstName}</p>
       <p>useAuth userId: {userId}</p>
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
--- app/(platform)/protected/server/page.tsx
+++ app/(platform)/protected/server/page.tsx
@@ -10,7 +10,7 @@ export default async function ProtectedServerPage() {
       <p>User: {user?.firstName}</p>
       <p>UserId: {userId}</p>
       <p>authUser: {authUser?.lastName}</p>
-      <UserButton />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
