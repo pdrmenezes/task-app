89155eb1a40f9be4c2da33cce9d971818be21823
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-21 19:10:01 -0300

    feat:setup template metadata

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
 import Link from "next/link";
-import { GeistSans } from "geist/font/sans";
 
 export default function MarketingPage() {
   return (
@@ -11,17 +10,14 @@ export default function MarketingPage() {
           <Medal className="mr-2 h-6 w-6" />
           Nº 1 task manager
         </div>
-        <h1 className="mb-6 text-center text-3xl text-neutral-800 md:text-6xl">
-          Taskify help teams move
-        </h1>
-        <div className="pb- rounded-md bg-gradient-to-r from-fuchsia-600 to-pink-600 px-4 py-2 text-3xl text-white md:text-6xl">
+        <h1 className="mb-6 text-center text-3xl font-bold text-neutral-800 md:text-6xl">Taskify help teams move</h1>
+        <div className="rounded-md bg-gradient-to-r from-fuchsia-600 to-pink-600 px-4 py-2 text-3xl text-white md:text-6xl">
           work forward.
         </div>
       </div>
       <div className="mx-auto mt-4 max-w-xs text-center text-sm text-neutral-400 md:max-w-2xl md:text-xl">
-        Collaborate, manage projects, and reach new productivity peaks. From
-        high rises to the home office, the way your team works is unique -
-        accomplish it all with Taskify
+        Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your
+        team works is unique - accomplish it all with Taskify
       </div>
       <Button className="mt-6 ">
         <Link href="/sign-up">Get Taskify for free</Link>
